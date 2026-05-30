891c8e539b7da007983732f50ba346b3b9a1823b
Sofia Marijuan Carreno <marijuas@lsbu.ac.uk>
2026-05-30 20:37:42 +0100

    Created model.js file.

---
+++ __tests__/model.test.js
@@ -0,0 +1,43 @@
+const { addTask, toggleTask, clearCompletedTask } = require('../model');
+
+describe('todo model', () => {
+    test('adds a task', () => {
+
+        const tasks = [];
+
+        const result = addTask(tasks, 'Buy milk');
+
+        expect(result).toEqual([
+            {text: 'Buy milk', completed: false}
+        ]);
+    });
+    test('does not mutate original array', () => {
+        
+        const tasks = [];
+
+        addTasks(tasks, 'Buy milk');
+
+        expect(tasks).toEqual([]);
+    });
+    test('toggles task', () => {
+        
+        const tasks = [{text: 'Buy milk, completed: false'}];
+
+        const result = toggleTask(tasks,0);
+
+        expect(result[0].completed).toBe(true);
+    });
+    test('clears completed tasks', () => {
+        
+        const tasks = [
+            {text: 'A', completed: true},
+            {text: 'B', competed: false}
+        ];
+
+        const result = clearCompletedTask(tasks);
+
+        expect(result).toEqual([
+            {text: 'B', completed: false}
+        ]);
+    });
+});
---
+++ model.js
@@ -0,0 +1,23 @@
+let tasks= [];
+function createTask(text) {
+    return {
+        text,
+        completed: false
+    };
+}
+
+function addTask(tasks, task) {
+    if (!text.trim()) return tasks;
+
+    return [...tasks, createTask(text)];
+}
+
+function toggleTask(tasks, index) {
+    return tasks.map((task, i) => 
+    i === index ? { ...task, completed: !task.completed } : task);
+}
+function clearCompletedTask(tasks) {
+    return tasks.filter(task => !task.completed);
+}
+
+module.exports = { addTask, toggleTask, clearCompletedTask }
