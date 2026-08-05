ef3f9ed903a87e1cf7ecfa6c91c8080178bae2e3
smc8501 <s.marijuancarreno9378@gmail.com>
2026-08-05 09:57:22 +0100

    Fix Bug

--- todolist/src/store.js
+++ todolist/src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from './Components/ToDoSlice';
+import todoReducer from './components/ToDoSlice';
 const store = configureStore({
     reducer: {
         todos: todoReducer,
